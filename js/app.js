0bc332251ad55cf93b8432f84519ac8b4bd3e61e
julius <njuliuskamau@gmail.com>
2017-12-05 19:33:30 +0300

    jk:full version

--- js/app.js
+++ js/app.js
@@ -19,14 +19,29 @@ PARALLAX = {
     bindParralax: function () {
         $(document).ready(function() {
             $('#fullpage').fullpage({
-              sectionsColor: ['#000', '#000', '#171717', '#000', '#000'],
-              anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5fifth'],
+              sectionsColor: ['#000', '#000', '#171717', '#000', '#000','#000'],
+              anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5fifthpage','6sixthpage'],
               menu: '#menu',
-              scrollBar: true,
-              scrollOverflow: true,
+              scrollBar: true, 
+              
+             
+              responsiveWidth: 900,
+              afterResponsive: function(isResponsive){
+              
+              }
+
               
               
 
+            });
+        });
+        
+    },
+    bindClassActive: function () {
+        $(function(){
+            $('.callender_wrapper .panel.relative').click(function(){
+                $('.callender_wrapper .panel.relative.active').removeClass('active');
+                $(this).addClass('active');
             });
         });
         
@@ -46,6 +61,7 @@ PARALLAX = {
 var onLoad = function () {
     LOADER.bindLoader();
     PARALLAX.bindParralax();
+    PARALLAX.bindClassActive();
  
 };
 
